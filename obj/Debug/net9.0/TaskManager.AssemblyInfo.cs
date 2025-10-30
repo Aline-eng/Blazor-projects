c5e65fb553ccc404c9dd599b830ecdbb40ecfe6b
Aline-eng <alinenzikwinkunda@gmail.com>
2025-10-30 22:55:01 +0200

    Merge remote repository initialization with local TaskManager project

--- obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5752e6884a90bf88735c326a6324dcddccf84eb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb2e33428a61b2331dc4908f09f5a7a884dac6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
